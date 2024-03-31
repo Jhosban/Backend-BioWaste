94deef22a057eed8a3a2b394fcdc38544a366000
Jhosban <jhosban8@gmail.com>
2024-03-31 00:53:54 -0500

    Cors agregado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
+import  cors  from "cors";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth-routes.js";
 
 
 const app = express();
 
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {login, registrer, logout, profile, adminRegistrer, createResidence} from '../controlers/auth-controller.js';
+import {login, registrer, logout, adminRegistrer, createResidence} from '../controlers/auth-controller.js';
 import { authRequired } from "../middelwares/validate-token.js";
 import { validateSchema } from "../middelwares/validator.middelware.js";
 import {registerSchema, loginSchema, adminSchema, residenceSchema} from "../schemas-validation/auth-schema.js"
@@ -11,6 +11,6 @@ router.post("/adminRegister", validateSchema(adminSchema), adminRegistrer);
 router.post('/login',validateSchema(loginSchema), login);
 router.post('/logout', logout);
 router.post('/createResidence',validateSchema(residenceSchema), createResidence);
-router.get('/profile', authRequired,  profile);
+
 
 export default router; 
