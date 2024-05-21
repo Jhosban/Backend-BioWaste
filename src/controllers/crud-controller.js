26f9ea74aedfeb3376ebd835ba02be0e670e94d9
Jhosban <jhosban8@gmail.com>
2024-05-21 10:27:23 -0500

    Actualizando el CRUD

--- src/controllers/crud-controller.js
+++ src/controllers/crud-controller.js
@@ -18,7 +18,8 @@ export const showUsers = async (req, res) => {
 export const deleteUser = async (req, res) => {
   try {
     const userId = req.params.id;
-    await User.deleteOne({ _id: userId });
+    
+    await User.findByIdAndDelete({ _id: userId });
 
     res.status(200).json({ message: "User deleted successfully" });
   } catch (err) {
@@ -36,22 +37,39 @@ export const showResidences = async (req, res) => {
   }
 };
 
-export const assignResidenceToUser = async (userId, residenceId) => {
+export const updateUser = async (req, res) => {
   try {
-    const residence = await Residence.find({ _id: residenceId });
+    const id = req.params.id;
 
-    if (!residence) {
-      throw new Error("Residence Not Found");
-    }
+    const userUpdated = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
 
-    if (User.residence) {
-      throw new Error("The user already has a residence assigned to them");
-    }
-
-    await User.findByIdAndUpdate(userId, { $set: { residence: residenceId } });
+    res.status(200).json(userUpdated);
   } catch (err) {
-    console.error(err);
+    res.status(500).json({ message: err.message });
   }
 };
 
-//console.log(assignResidenceToUser("664ba3fd2e8c74913157a5ab", 9069))
+export const assignResidenceToUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const residenceId = req.body.residenceId;
+
+    const residence = await Residence.find({ _id: residenceId });
+
+    if (!residence) {
+      res.status(404).json({ message: "Residence not found" });
+    }
+
+    if (User.residence) {
+      res.status(400).json({ message: "User already has a residence" });
+    }
+
+    await User.findByIdAndUpdate(userId, { $set: { residence: residenceId } });
+
+    res.status(200).json({ message: "Residence assigned successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- src/routes/crud-routes.js
+++ src/routes/crud-routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { showUsers, deleteUser, showResidences } from "../controllers/crud-controller.js";
+import { showUsers, deleteUser, showResidences, updateUser, assignResidenceToUser } from "../controllers/crud-controller.js";
 
 const router = Router()
 
 router.get('/showUsers/:id', showUsers)
-router.get('/deleteUser/:id', deleteUser)
+router.delete('/deleteUser/:id', deleteUser)
 router.get('/showResidence/:id', showResidences)
+router.put('/updateUser/:id', updateUser)
+router.post('/assignResidenceToUser/:id', assignResidenceToUser);
 
 export default router;  
