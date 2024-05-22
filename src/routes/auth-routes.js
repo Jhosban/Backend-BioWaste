7c9c00ecd44fbe83a8fb96310baaf9135145ccbf
Jhosban <jhosban8@gmail.com>
2024-05-22 10:32:59 -0500

    Eliminando tokens del proyecto 2

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {login, register , logout, adminRegister, createResidence, email_send} from '../controllers/auth-controllers.js';
+import {login, register , adminRegister, createResidence, email_send} from '../controllers/auth-controllers.js';
 import { validateSchema } from "../middlewares/middlewares.js";
 import {registerSchema, loginSchema, adminSchema, residenceSchema} from "../schemas-validation/auth-schema.js"
 
@@ -8,7 +8,6 @@ const router = Router()
 router.post('/register',validateSchema(registerSchema), register);
 router.post("/adminRegister", validateSchema(adminSchema), adminRegister);
 router.post('/login',validateSchema(loginSchema), login);
-router.post('/logout', logout);
 router.post('/createResidence',validateSchema(residenceSchema), createResidence);
 router.post('/email_send', email_send);
 
